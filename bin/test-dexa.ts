853a63ce067bf01e5b079e22d8ed2d43b43e3f8c
Travis Fischer <fisch0920@gmail.com>
2023-07-28 16:51:19 -0700

    🤺

--- bin/test-dexa.ts
+++ bin/test-dexa.ts
@@ -1,20 +1,12 @@
 import 'dotenv/config'
-import fs from 'fs/promises'
-const { pipeline } = require('stream');
-const { promisify } = require('util');
 
 import { DexaClient } from '@/index'
-
-const pipe = promisify(pipeline);
 
 async function main() {
   const dexa = new DexaClient()
 
   const res = await dexa.generateDexaAnswerFromLex('what is love?')
-
-  pipe(res, process.stdout);
-  process.stdout.write('\n');
-  // await fs.writeFile('test.mp3', res)
+  console.log(res)
 }
 
 main()
