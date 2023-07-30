16571dda5d303aac0f4f07aa891cf0f3ea0399b1
Travis Fischer <fisch0920@gmail.com>
2023-07-29 20:06:33 -0700

    📐

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import Link from 'next/link'
 import { useRouter } from 'next/router'
 import React, { useState } from 'react'
 import toast, { Toaster } from 'react-hot-toast'
+import Link from 'next/link'
 
 import { Layout } from '@/components/Layout/Layout'
 import { PageHead } from '@/components/PageHead/PageHead'
@@ -9,7 +9,7 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { getYoutubeMetadata } from '@/lib/api'
 
-const podcasters = [{ value: 'lex-fridman', label: 'Lex Fridman' }]
+import styles from './index.module.css'
 
 const IndexPage = () => {
   const router = useRouter()
@@ -27,7 +27,7 @@ const IndexPage = () => {
         const metadata = await getYoutubeMetadata(videoId)
 
         if (metadata == undefined) {
-          handleError('Please enter a valid Youtube URL. Metadata not found.')
+          handleError('Please enter a valid YouTube URL. Metadata not found.')
           return
         }
 
@@ -39,13 +39,13 @@ const IndexPage = () => {
           return
         }
       } catch {
-        handleError('Please enter a valid Youtube URL. Metadata fetch errored.')
+        handleError('Please enter a valid YouTube URL. Metadata fetch errored.')
         return
       }
 
       router.push(`/listen/${videoId}`)
     } else {
-      toast.error('Please enter a valid Youtube URL. URL not valid.')
+      toast.error('Please enter a valid YouTube URL. URL not valid.')
       return
     }
   }
@@ -81,20 +81,51 @@ const IndexPage = () => {
   return (
     <Layout>
       <PageHead />
-      <h2 className='text-2xl font-bold text-center mt-4'>
-        Welcome to Pickle Pod
-      </h2>
-      <p className='text-center mt-2 w-full'>Please enter the YouTube URL</p>
-      <Input
-        type='youtube'
-        className='mt-4 w-full p-2 border rounded'
-        value={url}
-        onChange={handleURLOnChange}
-        placeholder='YouTube URL'
-      />
-      <Button className='mt-4' variant='outline' onClick={handleStart}>
-        Go
-      </Button>
+
+      <div className={styles.body}>
+        <h2 className='text-2xl font-bold text-center mt-4'>
+          Welcome to Pickle Pod
+        </h2>
+
+        <p className='text-center mt-2 w-full'>
+          Please enter the URL of a YouTube podcast video you'd like to listen
+          and talk to.
+        </p>
+
+        <div className={styles.form}>
+          <Input
+            type='youtube'
+            className='mt-4 w-full p-2 border rounded'
+            value={url}
+            onChange={handleURLOnChange}
+            placeholder='YouTube URL'
+          />
+
+          <Button className='mt-4' variant='outline' onClick={handleStart}>
+            Go
+          </Button>
+        </div>
+
+        <div className='text-center mt-2 w-full'>
+          <p className={styles.example}>Or choose an example video:</p>
+          <Link className={styles.link} href='/listen/gk4tEO4jDUM'>
+            Lex & Joe Rogan
+          </Link>{' '}
+          |{' '}
+          <Link className={styles.link} href='/listen/DxREm3s1scA'>
+            Lex & Elon Musk
+          </Link>{' '}
+          |{' '}
+          <Link className={styles.link} href='/listen/4dC_nRYIDZU'>
+            Lex & Sam Harris
+          </Link>{' '}
+          |{' '}
+          <Link className={styles.link} href='/listen/Ff4fRgnuFgQ'>
+            Lex & Mark Zuckerberg
+          </Link>
+        </div>
+      </div>
+
       <Toaster />
     </Layout>
   )
